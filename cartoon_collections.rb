9618abbeeb74912775f4a85c57853b1f8f588374
Lisa Marie McGerr <lisamarie.mcgerr@gmail.com>
2020-11-28 21:31:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,15 +19,13 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
+  planeteer_calls.find do |valid_calls|
+    planeteer_calls.include?(valid_calls)
+    
   
 end
  
-when true_value = planeteer_calls.index(valid_calls)
-true_value
-elsif planeteer_calls.index(valid_calls) == nil
-  nil
-end
-end
+
 
  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
